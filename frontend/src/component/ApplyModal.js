0125998f7da316853dc5b2224329681afa03fcd6
quangtrin <tienvu123d@gmail.com>
2024-06-13 22:10:25 +0700

    fix height

--- frontend/src/component/ApplyModal.js
+++ frontend/src/component/ApplyModal.js
@@ -4,7 +4,7 @@ const ApplyModal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-1/2 h-1/2">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-1/2 h-3/5">
         <div className="flex justify-end pb-2">
           <button
             onClick={() => {
